dbbb1ae7d08ff385199eabd18dd870d681539673
Dennis <destronan5@gmail.com>
2023-09-02 01:28:24 +0300

    refactor(pages): optimize page component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Hero from "@/components/Home/Hero";
+import SearchInput from "@/components/Home/SearchInput";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello World</h2>
+    <div className="p-5 md:px-10 sm:px-20">
+      <Hero />
+      <SearchInput />
     </div>
   )
 }
