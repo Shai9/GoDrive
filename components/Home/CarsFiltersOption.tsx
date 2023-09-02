6cf5d5b2325b83738f0f8e9c8be66ade30f5f0f1
Dennis <destronan5@gmail.com>
2023-09-02 22:36:02 +0300

    Create the cars filter option card

---
+++ components/Home/CarsFiltersOption.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CarsFiltersOption() {
+  return (
+    <div className='mt-10'>
+      <div>
+        <h2 className='text-[30px] font-bold'>Cars Catalogue</h2>
+        <h2>Explore cars you might like</h2>
+      </div>
+    </div>
+  )
+}
+
+export default CarsFiltersOption
